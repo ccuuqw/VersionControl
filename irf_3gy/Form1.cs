5e2f6dca05334d4de18838c8f97af4a8a89ddf54
ccuuqw <etoforevva@gmail.com>
2020-09-26 23:02:15 +0200

    Osztály + Form

--- irf_3gy/Form1.cs
+++ irf_3gy/Form1.cs
@@ -15,6 +15,9 @@ namespace irf_3gy
         public Form1()
         {
             InitializeComponent();
+            lblLastName.Text = Resource1.LastName;
+            lblFirstName.Text = Resource1.FirstName;
+            btnAdd.Text = Resource1.Add;
         }
     }
 }
