cc3cf96651a542566539fa577e3feb6bf2020477
ccuuqw <etoforevva@gmail.com>
2020-09-27 00:15:35 +0200

    Mentés

--- irf_3gy/Form1.cs
+++ irf_3gy/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace irf_3gy
             InitializeComponent();
             lblFullName.Text = Resource1.FullName;
             btnAdd.Text = Resource1.Add;
+            btnFile.Text = Resource1.File;
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
             listUsers.DisplayMember = "FullName";
@@ -31,6 +33,32 @@ namespace irf_3gy
                 FullName = txtFullName.Text,
             };
             users.Add(u);
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            if (users.Count==0)
+            {
+                MessageBox.Show("Üres a lista!","Ajjaj!");
+                return;
+            }
+            MessageBox.Show("Minden oké!", "Nó jaj!");
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var nev in users)
+                {
+                    sw.Write(nev.ID.ToString());
+                    sw.Write(";");
+                    sw.Write(nev.FullName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
