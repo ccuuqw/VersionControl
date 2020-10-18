c4503cda17fa0d74642a491da3917ee103b24ee8
ccuuqw <etoforevva@gmail.com>
2020-10-18 21:36:52 +0200

    Diagram létrehozása

--- irf_6gy/Form1.cs
+++ irf_6gy/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace irf_6gy
@@ -31,6 +32,7 @@ namespace irf_6gy
             var result = response.GetExchangeRatesResult;
             dataGridView1.DataSource = Rates;
             newXml(result);
+            newChart();
         }
         void newXml(string result)
         {
@@ -47,6 +49,24 @@ namespace irf_6gy
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0) rate.Value = value / unit;
             }
+        }
+        void newChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
