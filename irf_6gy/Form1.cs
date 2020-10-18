9d0e712a9979dc524883f1c18aeaaa2ae5caecca
ccuuqw <etoforevva@gmail.com>
2020-10-18 20:40:28 +0200

    Webszolgáltatás lehívása

--- irf_6gy/Form1.cs
+++ irf_6gy/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using irf_6gy.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,21 @@ using System.Windows.Forms;
 
 namespace irf_6gy
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
