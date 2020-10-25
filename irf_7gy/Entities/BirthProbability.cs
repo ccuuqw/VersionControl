49d9dcd8dcdb636e288b6ecf0bfe71667f58ab4c
ccuuqw <etoforevva@gmail.com>
2020-10-25 21:51:31 +0100

    Osztályok létrehozása

---
+++ irf_7gy/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irf_7gy.Entities
+{
+    class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ irf_7gy/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irf_7gy.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int BirthYear { get; set; }
+        public double P { get; set; }
+    }
+}
